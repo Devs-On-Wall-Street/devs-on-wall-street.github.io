9bccc5d16e762664e50778bcc8408a963b1ac749
vkatari10 <kaskat2021@gmail.com>
2025-01-15 21:28:22 -0500

    Updated about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
+import img1 from '../imgs/members/image1.png'
+import img2 from '../imgs/members/image2.png'
+import img3 from '../imgs/members/image3.jpg'
+import img4 from '../imgs/members/image4.jpg'
+import img5 from '../imgs/members/image5.jpg'
+
 function AboutPage() {
   return (
     <div>
@@ -22,7 +28,7 @@ function AboutPage() {
               <Card.Body>
                 <Card.Title>Our Mission</Card.Title>
                 <Card.Text>
-                  At Devs on Wallstreet, we aim to empower students with the skills and knowledge needed to excel in the tech and finance industries. We organize workshops, hackathons, and networking events to foster growth and collaboration among our members.
+                  At Devs on Wallstreet, we aim to empower students with the skills and knowledge needed to excel in the tech and finance industries. We organize workshops, hackathons, and networking events to foster growth and collaboration among our members.-
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -38,14 +44,14 @@ function AboutPage() {
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src="https://via.placeholder.com/150" 
+                src={img1}
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
               <Card.Body>
-                <Card.Title>Team Member One</Card.Title>
+                <Card.Title>Saachi - Marketing Team</Card.Title>
                 <Card.Text>
-                  John Doe - President
+                I am a second year Computer Science major with a minor in business and a certificate in fintech. I’m interested in the applications of technology for finance and how they intersect. 
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -54,14 +60,14 @@ function AboutPage() {
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src="https://via.placeholder.com/150" 
+                src={img2}
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
               <Card.Body>
-                <Card.Title>Team Member Two</Card.Title>
+                <Card.Title>Eshan - Co-VP of External Affairs</Card.Title>
                 <Card.Text>
-                  Jane Smith - Vice President
+                My name is Eshan Bhimani, and I am a freshman studying Computer Science with a minor in math. I enjoy the intersection of tech and finance as I aspire to use algorithms to trade in public markets. Outside of academics, I enjoy playing tennis, going to the gym, and playing poker!
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -70,20 +76,63 @@ function AboutPage() {
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src="https://via.placeholder.com/150" 
+                src={img3} 
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
               <Card.Body>
-                <Card.Title>Team Member Three</Card.Title>
+                <Card.Title>Evan Hammam - VP</Card.Title>
                 <Card.Text>
-                  Emily Brown - Treasurer
+                I am a fourth year Computer Science major with a minor in math. I research computer vision applications for the university. Outside of programming I like going to the gym and playing instruments.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
       </Container>
+
+           {/* Team Section */}
+           <Container className="my-5">
+       
+        <Row>
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img4}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Apruva - VP of Technology</Card.Title>
+                <Card.Text>
+                I am a fourth year computer science major with an area of emphasis in applied data science and a minor in statistics. I am interested in the applications of data science within finance.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img5}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Vikas - Webmaster</Card.Title>
+                <Card.Text>
+                  I am a second year computer science and mathematics major. I'm interested in the intersection of computational investing and machine learning. 
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        
+          
+          
+        </Row>
+      </Container>
+
 
       {/* Join Us Section */}
       <Container fluid className="bg-secondary text-white text-center p-5">
