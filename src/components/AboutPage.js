9f0aa910c42e1f1a049acb9b3e142a6bb6358674
vkatari10 <kaskat2021@gmail.com>
2025-01-16 15:24:27 -0500

    Added more members to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
+/* Images of members */
 import img1 from '../imgs/members/image1.png'
 import img2 from '../imgs/members/image2.png'
 import img3 from '../imgs/members/image3.jpg'
 import img4 from '../imgs/members/image4.jpg'
 import img5 from '../imgs/members/image5.jpg'
+import img6 from '../imgs/members/image6.png'
+import img7 from '../imgs/members/image7.jpg'
+import img8 from '../imgs/members/image8.png'
+import img9 from '../imgs/members/image9.png'
+import img10 from '../imgs/members/image10.jpg'
 
 function AboutPage() {
   return (
@@ -76,7 +82,7 @@ function AboutPage() {
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src={img3} 
+                src={img5} 
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
@@ -91,15 +97,14 @@ function AboutPage() {
         </Row>
       </Container>
 
-           {/* Team Section */}
-           <Container className="my-5">
-       
+      {/* Team Section Row 2 */}
+        <Container className="my-5">
         <Row>
           <Col md={4}>
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src={img4}
+                src={img7}
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
@@ -115,7 +120,7 @@ function AboutPage() {
             <Card className="text-center">
               <Card.Img 
                 variant="top" 
-                src={img5}
+                src={img10}
                 alt="Team Member Image" 
                 style={{ width: '300px', height: '300px', margin: '15px auto' }}
               />
@@ -127,9 +132,107 @@ function AboutPage() {
               </Card.Body>
             </Card>
           </Col>
-        
-          
-          
+
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img9}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Arjun - Co-VP of External Affairs</Card.Title>
+                <Card.Text>
+                I’m a 4th year Computer Science Major. Interested in Machine Learning and Algorithmic Trading. Outside of school, I like to cook and go hiking.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+       
+      {/* Team Section Row 3 */}
+      <Container className="my-5">
+        <Row>
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img3}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Richard Phan - President</Card.Title>
+                <Card.Text>
+                I am a 2nd year CS major with an emphasis in AI. I care about career growth and early action to networking and employability. Outside of class, I enjoy going out, poker, and gaming.
+Incoming @ Amazon
+Prev @ McKenney’s, Verinext
+
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img6}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Aishu Kamaraj - Treasurer</Card.Title>
+                <Card.Text>
+                I am a 2nd-year Computer Science major with a minor in General Business. I am interested in financial cybersecurity. Outside of academics, I enjoy painting and reading.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img8}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Nikita - VP</Card.Title>
+                <Card.Text>
+                I am a third-year computer science and economics double major. I am interested in leveraging the power of data to solve challenging, real-world problems. In my free time, I like running, reading, and going to the gym. 
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Team Section Row 4 */}
+      <Container className="my-5">
+        <Row>
+          <Col md={4}>
+            <Card className="text-center">
+              <Card.Img 
+                variant="top" 
+                src={img4}
+                alt="Team Member Image" 
+                style={{ width: '300px', height: '300px', margin: '15px auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Anmol Matharu - Marketing Team</Card.Title>
+                <Card.Text>
+                I am a 2nd year Computer Science major with a certificate in Data Science. I am interested in algorithmic trading. Outside of class, I love to bake and cook.
+
+
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+         
         </Row>
       </Container>
 
